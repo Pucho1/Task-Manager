c291c1c642e90685fb1199ce9433a7fbc1acc436
Pucho <martinezochandarenam@gmail.com>
2026-05-01 12:12:57 +0200

    fix: filters position

--- src/features/tasks/components/FilterTask.tsx
+++ src/features/tasks/components/FilterTask.tsx
@@ -18,10 +18,10 @@ const FilterTask = <T extends string>({
   const selectId = useId();
 
   return (
-    <div className="w-1/3">
+    <div className="flex flex-col w-1/3 mr-2 justify-start">
       <label
         htmlFor={selectId}
-        className="flex sm:inline text-sm font-medium text-gray-600 mr-2 min-w-max dark:text-gray-300"
+        className="text-sm font-medium text-gray-600 mr-2 min-w-max dark:text-gray-300 text-left"
       >
         {label}
       </label>
@@ -29,7 +29,7 @@ const FilterTask = <T extends string>({
         id={selectId}
         value={valueFilter}
         onChange={(e) => handlerFilter(e.target.value as T)}
-        className="border min-w-[130px] text-black px-3 py-2 rounded-lg bg-white shadow-sm focus:ring-2 focus:ring-indigo-500 outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
+        className="border min-w-[115px] max-w-[150px] text-black px-3 py-2 rounded-lg bg-white shadow-sm focus:ring-2 focus:ring-indigo-500 outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
       >
         {options.map((opt) => (
           <option key={opt.value} value={opt.value}>
